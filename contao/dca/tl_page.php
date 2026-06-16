cc0aff55d908af7421c8a5564d7b9694975f8a01
Christian Barkowsky <christian@plenta.io>
2026-06-16 13:15:18 +0200

    DCA improvements

--- contao/dca/tl_page.php
+++ contao/dca/tl_page.php
@@ -22,7 +22,7 @@ $GLOBALS['TL_DCA']['tl_page']['fields']['page_status'] = [
     'inputType' => 'select',
     'filter' => true,
     'foreignKey' => 'tl_page_status.name',
-    'eval' => ['maxlength' => 255, 'tl_class' => 'w50', 'includeBlankOption' => true, 'chosen' => true],
+    'eval' => ['tl_class' => 'w50', 'includeBlankOption' => true, 'chosen' => true],
     'sql' => "int(10) unsigned NOT NULL default 0",
     'relation' =>['type' => 'belongsTo', 'load' => 'lazy'],
 ];
